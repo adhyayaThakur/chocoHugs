c27f1a7fb94b07360acb54f47e960cc0d72bdd19
unthinkable-lap-0255 <adhyayathakur240499@gmail.com>
2022-03-08 20:59:22 +0530

    to save work

---
+++ views/loginEmailMessage/loginEmailMessage.js
@@ -0,0 +1,11 @@
+const loginEmailMessage = `
+<html>
+<body style={{
+    display:flex,
+    justifyContent: 'center',
+    alignItems:'center',
+}}>
+<div>Congraltions! Just a last step to activate account</div>
+<button onClick={}>Click here</button>
+<body>
+<html>`
